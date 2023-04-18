8c85b88e2bcd4141db59723a7921cad864f9ecf2
Arseniy Parakhin <senya.paraxin.04@mail.ru>
2023-04-19 02:01:50 +0300

    bool + set + queue

--- Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
+++ Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
@@ -8,11 +8,47 @@
 
 #include <iostream>
 #include <cmath>
+#include <queue>
+#include <set>
 using namespace std;
 
 bool arr[3002][3002];
+bool flag = 1;
+queue <pair<int,int>> Q;
+set <pair<int, int>> marked;
 
-void Make_Cell_Value(int a, int b) {
+void cell_processing(int a, int b) {
+
+	if (arr[a][b] == 1) {
+		if (arr[a-1][b] == 0) {
+			Q.push({a - 1,b});
+			marked.insert({ a,b });
+		}
+		if (arr[a][b-1] == 0) {
+			Q.push({a,b-1});
+			marked.insert({ a,b });
+		}
+		if (arr[a+1][b] == 0) {
+			Q.push({a+1,b});
+			marked.insert({ a,b });
+		}
+		if (arr[a][b+1] == 0) {
+			Q.push({a,b+1});
+			marked.insert({ a,b });
+		}
+	}
+
+	return;
+}
+
+void pop_all_queue() {
+
+	while (!Q.empty()){
+		arr[Q.front().first][Q.front().second] = 1;
+		flag = 1;
+		
+		Q.pop();
+	}
 
 	return;
 }
@@ -22,10 +58,10 @@ int main()
 	int N, M;
 	cin >> N >> M;
 
+	// Заполнение стен
 	for (int x = 0; x < N + 2; x++) {
 		for (int y = 0; y < M + 2; y++) {
 			if ((x == 0 || y == 0) || (x == N + 1 || y == M + 1)) {
-				arr[x][y] = 1;
 				arr[x][y] = 1;
 			}
 		}
@@ -39,7 +75,33 @@ int main()
 		arr[x][y] = 1;
 	}
 
+	int cnt = -1;
+	while (flag == 1) {
+
+		for (int x = 1; x <= N ; x++) {
+			for (int y = 1; y <= M; y++) {
+				if (!marked.count({x,y})) {
+					cell_processing(x, y);
+				}
+			}
+		}
+
+		flag = 0;
+		pop_all_queue();
+		cnt++;
+		cout << endl << endl;
+
+	}
 
 
+	//for (int x = 1; x <= N; x++) {
+	//	for (int y = 1; y <= M; y++) {
+	//		cout << arr[x][y] << ' ';
+	//	}
+	//	cout << endl;
+	//}
+
+
+	cout << cnt << endl;
 	return 0;
 }
