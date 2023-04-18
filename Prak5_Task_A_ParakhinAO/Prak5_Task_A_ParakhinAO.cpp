e6a2661ce62b26b20c74f128e237459d6989ae16
Arseniy Parakhin <senya.paraxin.04@mail.ru>
2023-04-19 00:49:07 +0300

    Test 1

--- Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
+++ Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
@@ -7,20 +7,14 @@
 */
 
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
-typedef int value;
-int Max = 0;
 
-value Make_Cell_Value(int a, int b, vector<vector<value>>&V) {
-	if (V[a][b] == 0) {
-		return 0;
-	}
+bool arr[3002][3002];
 
-	int result = 1 + min(min(V[a-1][b], V[a+1][b]),min(V[a][b-1], V[a][b+1]));
+void Make_Cell_Value(int a, int b) {
 
-	return result;
+	return;
 }
 
 int main()
@@ -28,11 +22,12 @@ int main()
 	int N, M;
 	cin >> N >> M;
 
-	vector<vector<value>>V(N + 2, vector<value>(M + 2, 8888));
-	
-	for (int x = 1; x <= N; x++) {
-		for (int y = 1; y <= M; y++) {
-			V[x][y] = 9999;
+	for (int x = 0; x < N + 2; x++) {
+		for (int y = 0; y < M + 2; y++) {
+			if ((x == 0 || y == 0) || (x == N + 1 || y == M + 1)) {
+				arr[x][y] = 1;
+				arr[x][y] = 1;
+			}
 		}
 	}
 
@@ -41,38 +36,10 @@ int main()
 	cin >> quantity;
 	for (int x = 0, y = 0, i = 0; i < quantity; i++) {
 		cin >> x >> y;
-		V[x][y] = 0;
+		arr[x][y] = 1;
 	}
 
-	for (int x = 1; x <= N; x++) {
-		for (int y = 1; y <= M; y++) {
-			V[x][y] = Make_Cell_Value(x, y, V);
-		}
-	}
 
-	for (int x = N; x > 0; x--) {
-		for (int y = M; y > 0; y--) {
-			V[x][y] = Make_Cell_Value(x, y, V);
-		}
-	}
-
-	for (int x = 1; x <= N; x++) {
-		for (int y = 1; y <= M; y++) {
-			if (V[x][y] > Max) {
-				Max = V[x][y];
-			}
-		}
-	}
-	
-	//// Проверка выводом
-	//for (int x = 0; x < N+2; x++) {
-	//	for (int y = 0; y < M+2; y++) {
-	//		cout << ' ' << V[x][y];
-	//	}
-	//	cout << endl;
-	//}
-
-	cout << Max << endl;
 
 	return 0;
 }
