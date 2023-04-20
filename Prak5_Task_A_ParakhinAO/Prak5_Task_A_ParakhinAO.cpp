6d07cfb353b675072e7736ef20b5719831ae3693
Arseniy Parakhin <senya.paraxin.04@mail.ru>
2023-04-20 15:09:03 +0300

    Accepted

--- Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
+++ Prak5_Task_A_ParakhinAO/Prak5_Task_A_ParakhinAO.cpp
@@ -3,46 +3,42 @@
 Структуры и алгоритмы обработки данных
 Парахин А.О. КТбо1-7
 Практика 5 Задание A
-18.04.2023
+20.04.2023
 */
 
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 #include <queue>
 #include <set>
 using namespace std;
 
 bool arr[3002][3002];
 bool flag = 1;
-
-vector <pair<int, int>> marked;
+int Sum;
+queue <pair<int, int>> Q;
+set <pair<int, int>> S;
 
 void cell_processing(int a, int b) {
 
-	if (arr[a][b] == 1) {
-		if (arr[a-1][b] == 0 && std::find(marked.begin() + 1, marked.end(), make_pair(a - 1, b)) == marked.end()) {
-			marked.push_back({a - 1,b});
-		}
-		if (arr[a][b-1] == 0 && std::find(marked.begin() + 1, marked.end(), make_pair(a, b - 1)) == marked.end()) {
-			marked.push_back({a,b-1});
-		}
-		if (arr[a+1][b] == 0 && std::find(marked.begin() + 1, marked.end(), make_pair(a+1, b)) == marked.end()) {
-			marked.push_back({a+1,b});
-		}
-		if (arr[a][b+1] == 0 && std::find(marked.begin() + 1, marked.end(), make_pair(a, b+1)) == marked.end()) {
-			marked.push_back({a,b+1});
-		}
+	if (arr[a-1][b] == 0) {
+		Q.push({a - 1,b});
+		arr[a-1][b] = 1;
+		Sum--;
 	}
-
-	return;
-}
-
-void pop_all_set() {
-
-	for (auto it = marked.begin(); it != marked.end(); it++) {
-		arr[(*it).first][(*it).second] = 1;
-		flag = 1;
+	if (arr[a][b-1] == 0) {
+		Q.push({a,b-1});
+		arr[a][b-1] = 1;
+		Sum--;
+	}
+	if (arr[a+1][b] == 0) {
+		Q.push({a+1,b});
+		arr[a+1][b] = 1;
+		Sum--;
+	}
+	if (arr[a][b+1] == 0) {
+		Q.push({a,b+1});
+		arr[a][b+1] = 1;
+		Sum--;
 	}
 
 	return;
@@ -52,6 +48,7 @@ int main()
 {
 	int N, M;
 	cin >> N >> M;
+	Sum = N * M;
 
 	// Заполнение стен
 	for (int x = 0; x < N + 2; x++) {
@@ -67,30 +64,27 @@ int main()
 	cin >> quantity;
 	for (int x = 0, y = 0, i = 0; i < quantity; i++) {
 		cin >> x >> y;
-		arr[x][y] = 1;
-		marked.push_back({ x, y });
+		if (S.count({ x,y }) == 0) {
+			S.insert({ x,y });
+			Q.push({ x, y });
+			arr[x][y] = 1;
+
+			Sum--;
+		}
 	}
 
-	int cnt = -1, len;
-	while (flag == 1) {
-		len = marked.size();
+	int cnt = 0, len;
+
+	while (Sum > 0) {
+		len = Q.size();
+		
 		for (int i = 0; i < len; i++) {
-			int x = marked[0].first, y = marked[0].second;
-			
-			cell_processing(x, y);
-			marked.erase(marked.begin());
+			cell_processing(Q.front().first, Q.front().second);
+			Q.pop();
 		}
 
-		flag = 0;
-		for (int x = 1; x <= N; x++) {
-			for (int y = 1; y <= M; y++) {
-				cout << arr[x][y] << ' ';
-			}
-			cout << endl;
-		}
-		cout << endl;
-		pop_all_set();
 		cnt++;
+
 	}
 
 	cout << cnt << endl;
